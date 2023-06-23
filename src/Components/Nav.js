cc9a39ad726ffeab8a8157b382a06eddb45af338
Mohammed <malfaris4771@gmail.com>
2023-06-23 17:05:29 +0300

    Installed react-router-dom, tailwindCSS and framer-motion

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,21 +1,33 @@
 import React from "react";
-import Logo from "../assets/icons/Logo.svg"
+import Logo from "../assets/icons/Logo.svg";
 
 function Nav() {
   return (
-  <nav>
-    <div className="logo">
-      <img src={Logo} alt="" />
-    </div>
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Menu</a></li>
-      <li><a href="#">Reservations</a></li>
-      <li><a href="#">Order Online</a></li>
-      <li><a href="#">Login</a></li>
-    </ul>
-  </nav>
+    <nav>
+      <div className="logo">
+        <img src={Logo} alt="" />
+      </div>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">Reservations</a>
+        </li>
+        <li>
+          <a href="#">Order Online</a>
+        </li>
+        <li>
+          <a href="#">Login</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
